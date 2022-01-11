3affc33b03eee775028b0596470309557f902fff
geramazen <92953436+geramazen@users.noreply.github.com>
2022-01-12 01:50:12 +0200

    Add project files.

---
+++ GuestBook/Controllers/usersController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GuestBook.Models;
+
+namespace GuestBook.Controllers
+{
+    public class usersController : Controller
+    {
+        private GuestBookEntities db = new GuestBookEntities();
+
+        [HttpGet]
+        [ActionName("Registar")]
+        public ActionResult Registar_Get()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Registar")]
+        public ActionResult Registar_Post([Bind(Include = "email,password")] user user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("index");
+            }
+            return View(user);
+        }
+
+        [HttpGet, ActionName("Login")]
+        public ActionResult Login_get()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("Login")]
+        public ActionResult Login_post([Bind(Include = "email,password")] user user)
+        {
+            var rec = db.users.Where(x => x.email == user.email && x.password == user.password).ToList().FirstOrDefault();
+            if (rec != null)
+            {
+                Session["email"] = rec.email;
+                return RedirectToAction("ViewMessages", "messages");
+            }
+            else
+            {
+                ViewBag.error = "invalid user";
+                return View(user);
+
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session["email"] = null;
+            return RedirectToAction("Login");
+        }
+       
+
+    }
+}
---
+++ GuestBook/Models/UserCustomize.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace GuestBook.Models
+{
+    [MetadataType(typeof(UserMetaData))]
+    public partial class user
+    {
+      
+    }
+
+    public class UserMetaData
+    {
+        public int UID { get; set; }
+        [Required]
+        //[RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", ErrorMessage = "Email is Not Correct")]
+        public string email { get; set; }
+        [Required]
+        //[RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$", ErrorMessage = "Password is Not Strong")]
+        public string password { get; set; }
+    }
+}
